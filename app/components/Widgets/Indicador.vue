1e34decefcd31c812c1672ea85b274c8f78675a7
mreyesba1983 <mreyesba@gmail.com>
2022-08-20 13:09:56 -0500

    Componente indicador.vue usamos un icono tipo circulo

--- app/components/Widgets/Indicador.vue
+++ app/components/Widgets/Indicador.vue
@@ -4,30 +4,44 @@
             <h4 class="card-title">{{config.selectedDevice.name}} - {{config.variableFullName}}</h4>
         </div>
 
-        <i class="fa " :class="[config.icon]"></i>
+        <i class="fa " :class="[config.icon, getIconColorClass()]" style="font-size: 60px"></i>
     </card>
 </template>
 
 <script>
     export default {
-        //props:['config'],
+        props:['config'],
         data() {
             return {
-                config: {
-                    userId: "userid",
-                    selectedDevice: {
-                        name: "Home",
-                        dId: "1234",
-                        templateName: "Sensores",
-                        templateId: "1234567890abcdef",
-                        saverRule: "false"
-                    },
-                    variableFullName: "Temperatura",
-                    variable:"1234Temperatura",
-                    icon: "fa-sun",
-                    column: 'col-6',
-                    widget: 'indicator',
-                    class: 'danger'
+                value: false
+            }
+        },
+        mounted() {
+            const topic = this.config.userId + "/" + this.config.selectedDevice.dId + "/" + this.config.variable + "/sdata";
+            console.log(topic);
+            this.$nuxt.$on(topic, this.processReceivedData);
+        },
+        methods: {
+            processReceivedData(data) {
+                console.log("received");
+                console.log(data);
+                this.value = data.value;
+            },
+            getIconColorClass() {
+                if (!this.value) {
+                    return "text-dark";
+                }
+                if (this.config.class == "danger") {
+                    return "text-danger";
+                }
+                if (this.config.class == "warning") {
+                    return "text-warning";
+                }
+                if (this.config.class == "primary") {
+                    return "text-primary";
+                }
+                if (this.config.class == "success") {
+                    return "text-success";
                 }
             }
         }
--- app/pages/templates.vue
+++ app/pages/templates.vue
@@ -1,5 +1,82 @@
 <template>
-    <div>
-        <Indicador></Indicador>
+    <div class="row">
+        <div class="col-4">
+            <Indicador :config="config1"></Indicador>
+        </div>
+        <div class="col-4">
+            <Indicador :config="config2"></Indicador>
+        </div>
+        <div class="col-4">
+            <Indicador :config="config3"></Indicador>
+        </div>
+        
+        <button @click="sendData()">SEND</button>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                value: false,
+                config1: {
+                    userId: "userid",
+                    selectedDevice: {
+                        name: "Home",
+                        dId: "1234",
+                        templateName: "Sensores",
+                        templateId: "1234567890abcdef",
+                        saverRule: "false"
+                    },
+                    variableFullName: "Temperatura",
+                    variable:"Var1",
+                    icon: "fa-circle",
+                    column: 'col-6',
+                    widget: 'indicator',
+                    class: 'danger'
+                },
+                config2: {
+                    userId: "userid",
+                    selectedDevice: {
+                        name: "Home",
+                        dId: "1234",
+                        templateName: "Sensores",
+                        templateId: "1234567890abcdef",
+                        saverRule: "false"
+                    },
+                    variableFullName: "Humedad",
+                    variable:"Var2",
+                    icon: "fa-circle",
+                    column: 'col-6',
+                    widget: 'indicator',
+                    class: 'danger'
+                },
+                config3: {
+                    userId: "userid",
+                    selectedDevice: {
+                        name: "Home",
+                        dId: "1234",
+                        templateName: "Sensores",
+                        templateId: "1234567890abcdef",
+                        saverRule: "false"
+                    },
+                    variableFullName: "Velocidad",
+                    variable:"Var3",
+                    icon: "fa-circle",
+                    column: 'col-6',
+                    widget: 'indicator',
+                    class: 'danger'
+                }
+            }
+        },
+        methods: {
+            sendData() {
+                this.value = !this.value;
+                const toSend = {
+                    value: this.value
+                }
+                this.$nuxt.$emit("userid/1234/Var1/sdata", toSend);
+            }
+        }
+    }
+</script>
